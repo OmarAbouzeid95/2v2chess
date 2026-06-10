c23ff53c8f41811d4d019e985a4a834eee47af61
Omar Abouzeid <omaradham1995@gmail.com>
2026-06-10 15:22:58 -0400

    feat: install chess.js and set a random game

--- client/src/components/ChessBoard.tsx
+++ client/src/components/ChessBoard.tsx
@@ -1,10 +1,75 @@
-import { Chessboard, type ChessboardOptions } from 'react-chessboard';
-
-const options: ChessboardOptions = {
-	allowDragging: true,
-	boardOrientation: 'white',
-};
+import {
+	Chessboard,
+	type ChessboardOptions,
+	type PieceDropHandlerArgs,
+} from 'react-chessboard';
+import { Chess } from 'chess.js';
+import { useRef, useState } from 'react';
 
 export default function ChessBoard() {
-	return <Chessboard options={options} />;
+	const chessGameRef = useRef(new Chess());
+	const chessGame = chessGameRef.current;
+
+	// track the current position of the chess game in state to trigger a re-render of the chessboard
+	const [chessPosition, setChessPosition] = useState(chessGame.fen());
+
+	// make a random "CPU" move
+	function makeRandomMove() {
+		// get all possible moves`
+		const possibleMoves = chessGame.moves();
+
+		// exit if the game is over
+		if (chessGame.isGameOver()) {
+			return;
+		}
+
+		// pick a random move
+		const randomMove =
+			possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+
+		// make the move
+		chessGame.move(randomMove);
+
+		// update the position state
+		setChessPosition(chessGame.fen());
+	}
+
+	// handle piece drop
+	function onPieceDrop({ sourceSquare, targetSquare }: PieceDropHandlerArgs) {
+		// type narrow targetSquare potentially being null (e.g. if dropped off board)
+		if (!targetSquare) {
+			return false;
+		}
+
+		// try to make the move according to chess.js logic
+		try {
+			chessGame.move({
+				from: sourceSquare,
+				to: targetSquare,
+				promotion: 'q', // always promote to a queen for example simplicity
+			});
+
+			// update the position state upon successful move to trigger a re-render of the chessboard
+			setChessPosition(chessGame.fen());
+
+			// make random cpu move after a short delay
+			setTimeout(makeRandomMove, 50);
+
+			// return true as the move was successful
+			return true;
+		} catch (error) {
+			// return false as the move was not successful
+			console.log(error);
+			return false;
+		}
+	}
+
+	// set the chessboard options
+	const chessboardOptions: ChessboardOptions = {
+		position: chessPosition,
+		onPieceDrop,
+		id: 'play-vs-random',
+	};
+
+	return <Chessboard options={chessboardOptions} />;
 }
