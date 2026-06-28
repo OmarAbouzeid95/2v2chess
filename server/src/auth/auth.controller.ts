479b6a4dc4d7f4d97acad7b992b51701577a9b62
Omar Abouzeid <omaradham1995@gmail.com>
2026-06-27 23:26:39 -0400

    chore: add refresh-token endpoint

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -7,16 +7,21 @@ import {
   Post,
   Request,
   Res,
+  UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from '@/auth/auth.guard';
 import { AuthService } from '@/auth/auth.service';
 import { GenerateTokenDto } from './dto/generate-token-dto';
 import { type Response } from 'express';
+import { JwtService } from '@nestjs/jwt/dist/jwt.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @HttpCode(HttpStatus.OK)
   @Post('generate-token')
@@ -38,5 +43,29 @@ export class AuthController {
   @Get('verify-token')
   verifyToken(@Request() req) {
     return 'successfully verified token';
+  }
+
+  @Post('refresh-token')
+  async refreshToken(
+    @Request() req,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refresh_token } = req.cookies;
+
+    if (!refresh_token) {
+      throw new UnauthorizedException('Refresh token not found');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(refresh_token);
+
+      const { access_token, refresh_token: newRefreshToken } =
+        await this.authService.generateToken(payload.name, payload.uuid);
+
+      res.cookie('refresh_token', newRefreshToken, { httpOnly: true });
+      return { access_token, refresh_token: newRefreshToken };
+    } catch {
+      throw new UnauthorizedException();
+    }
   }
 }
